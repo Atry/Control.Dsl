5a30adae15430b4a02139bdf461cc3cff0a5082a
Yang Bo <atryyang@thoughtworks.com>
2018-09-28 02:07:59 +0800

    Add State Dsl

--- src/Control/Dsl/Internal.hs
+++ src/Control/Dsl/Internal.hs
@@ -7,3 +7,6 @@ class Dsl m a d where
   (>>=) :: m a -> (a -> d) -> d
   (>>) :: m a -> d -> d
   (>>) ma d = ma Control.Dsl.Internal.>>= \a -> d
+
+instance {-# OVERLAPPABLE #-} Dsl m a d => Dsl m a (b -> d) where
+  (>>=) k handler state = k Control.Dsl.Internal.>>= \x -> handler x state
---
+++ src/Control/Dsl/State.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE CPP #-}
+
+{- |
+Description : Mutable variables
+
+This @State@ module provides the ability to 'Put' and 'Get' the value of multiple mutable variables in a @do@ block.
+
+>>> :set -XTypeApplications
+>>> :set -XRebindableSyntax
+>>> import Prelude hiding ((>>), (>>=), return)
+>>> import Control.Dsl
+>>> import Data.Sequence
+>>> :{
+formatter :: Double -> Integer -> Seq String -> String
+formatter = do
+  --
+  -- Equivalent of `!Put(!Get[Vector[Any]] :+ "x=")` in Dsl.scala
+  tmpBuffer0 <- Get @(Seq String)
+  Put $ tmpBuffer0 |> "x="
+  --
+  -- Equivalent of `!Put(!Get[Vector[Any]] :+ !Get[Double])` in Dsl.scala
+  tmpBuffer1 <- Get @(Seq String)
+  d <- Get @Double
+  Put $ tmpBuffer1 |> show d
+  --
+  -- Equivalent of `!Put(!Get[Vector[Any]] :+ ",y=")` in Dsl.scala
+  tmpBuffer2 <- Get @(Seq String)
+  Put $ tmpBuffer2 |> ",y="
+  --
+  -- Equivalent of `!Put(!Get[Vector[Any]] :+ !Get[Int])` in Dsl.scala
+  tmpBuffer3 <- Get @(Seq String)
+  i <- Get @Integer
+  Put $ tmpBuffer3 |>  show i
+  --
+  -- Equivalent of `!Return((!Get[Vector[Any]]).mkString)` in Dsl.scala
+  tmpBuffer4 <- Get @(Seq String)
+  return $ foldl1 (++) tmpBuffer4
+:}
+
+>>> formatter 0.5 42 Empty
+"x=0.5,y=42"
+-}
+module Control.Dsl.State where
+
+import Prelude hiding ((>>), (>>=), return)
+import Control.Dsl.Internal
+
+data Put a b where
+  Put :: a -> Put a ()
+
+instance Dsl (Put a) () (a -> b) where
+  (>>=) (Put x) handler y = handler () x
+
+data Get a = Get
+
+instance Dsl Get a (a -> b) where
+  (>>=) Get handler x = handler x x
