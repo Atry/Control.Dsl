39ea77492d93f17fd401e27b64ab8f7e1cb4e33f
Yang Bo <atryyang@thoughtworks.com>
2018-09-29 15:55:57 +0800

    Add Empty Dsl

---
+++ src/Control/Dsl/Empty.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+module Control.Dsl.Empty where
+
+import Control.Dsl.Internal
+import Control.Applicative
+import Prelude (Monoid, mempty)
+
+data Empty a = Empty
+
+instance {-# OVERLAPPABLE #-} Monoid d => Dsl Empty a d where
+  (>>=) Empty _ = mempty
+
+instance {-# INCOHERENT #-} Alternative m => Dsl Empty a (m b) where
+  (>>=) Empty _ = empty
