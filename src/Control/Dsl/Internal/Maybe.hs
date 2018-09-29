14624f4cee60d8998b4d57e3fec5f4f7476bcdda
Yang Bo <atryyang@thoughtworks.com>
2018-09-29 15:55:00 +0800

    Add tests for Maybe Dsl

--- src/Control/Dsl/Internal/Maybe.hs
+++ src/Control/Dsl/Internal/Maybe.hs
@@ -11,6 +11,65 @@ import Prelude hiding ((>>), (>>=), return)
 import Control.Dsl.Return
 import Control.Dsl.Internal
 
+{- |
+This instance automatically lift a 'Maybe' to other domains,
+similar to @MaybeT@ but simpler.
+
+=== Examples
+
+Suppose I want to create a command line tool,
+which read a file and parse it as integers, then returns the sum.
+
+>>> :set -XTypeApplications
+>>> :set -XRebindableSyntax
+>>> import Prelude hiding ((>>), (>>=), return)
+>>> import Control.Dsl
+>>> import Text.Read
+>>> import System.IO
+>>> :{
+calculator :: Handle -> IO (Maybe Integer)
+calculator h = do
+  putStrLn "Input the first number..."
+  line1 <- hGetLine h
+  number1 <- readMaybe @Integer line1
+  putStrLn "Input the second number..."
+  line2 <- hGetLine h
+  number2 <- readMaybe @Integer line2
+  return (number1 + number2)
+:}
+
+
+When the input is correct, it should compute the sum of the input.
+
+>>> import System.IO.Temp
+>>> :{
+withSystemTempFile "tmp" $ \_ -> \h -> do
+  hPutStrLn h "40"
+  hPutStrLn h "2"
+  hSeek h AbsoluteSeek 0
+  calculator h
+:}
+Input the first number...
+Input the second number...
+Just 42
+
+When the input is in wrong format,
+it should return an @IO Nothing@.
+
+>>> import System.IO.Temp
+>>> :{
+withSystemTempFile "tmp" $ \_ -> \h -> do
+  hPutStrLn h "not a number"
+  hPutStrLn h "2"
+  hSeek h AbsoluteSeek 0
+  calculator h
+:}
+Input the first number...
+Nothing
+
+Note that the same functionality implemented in @MaybeT@ monad transformers
+would be much complicated due to manually @lift@ calls.
+-}
 instance Dsl (Return (Maybe a)) d d => Dsl Maybe a d where
   Nothing >>= f = return @(Maybe a) Nothing
   Just a >>= f = f a
