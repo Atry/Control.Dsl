3d16b3c163041bf08ea15500e2b0477d5d2390b8
Yang Bo <atryyang@thoughtworks.com>
2018-10-03 02:33:08 +0800

    Improve documentation

--- src/Control/Dsl.hs
+++ src/Control/Dsl.hs
@@ -4,71 +4,14 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 
-{- |
->>> :set -XTypeFamilies
->>> :set -XMultiParamTypeClasses
->>> :set -XFlexibleInstances
->>> :set -XFlexibleContexts
->>> :set -XRebindableSyntax
->>> :set -XTypeApplications
->>> import qualified Prelude
->>> import Prelude hiding ((>>), (>>=), return)
->>> import Control.Dsl
->>> import Control.Dsl.State
->>> import Control.Dsl.Yield
->>> import Control.Dsl.Return
->>> import Data.Void
-
->>> :{
-f = do
-  Yield "foo"
-  config <- Get @Bool
-  when config $ do
-    Yield "bar"
-    ($ ())
-  return "baz"
-:}
-
->>> :type f
-f :: (PolyCont (Yield [Char]) r (),
-      PolyCont (Return [Char]) r Void, PolyCont Get r Bool) =>
-     r
-
->>> f True :: [String]
-["foo","bar","baz"]
-
->>> f False :: [String]
-["foo","baz"]
-
->>> :{
-instance PolyCont (Yield String) (IO ()) () where
-  runPolyCont (Yield a) = (Prelude.>>=) (putStrLn $ "Yield " ++ a)
-:}
-
->>> :{
-instance PolyCont Get (IO ()) Bool where
-  runPolyCont Get f = (putStrLn "Get") Prelude.>> f False
-:}
-
->>> :{
-instance PolyCont (Return String) (IO ()) Void where
-  runPolyCont (Return a) _ = putStrLn $ "Return " ++ a
-:}
-
->>> f :: IO ()
-Yield foo
-Get
-Return baz
--}
 module Control.Dsl(
-  module Control.Dsl.Return,
   module Control.Dsl.Dsl,
+  module Control.Dsl.Return,
   module Control.Dsl.Empty,
   module Control.Dsl.Cont
 ) where
 
+import Control.Dsl.Dsl
 import Control.Dsl.Return (return)
 import Control.Dsl.Cont (when, unless)
-import Control.Dsl.Dsl
 import Control.Dsl.Empty (guard)
-import Prelude hiding ((>>), (>>=), return)
--- src/Control/Dsl/Dsl.hs
+++ src/Control/Dsl/Dsl.hs
@@ -10,24 +10,81 @@ import Control.Dsl.PolyCont
 import Control.Dsl.Cont
 import Prelude hiding ((>>), (>>=), return)
 
-{- An use case of a keyword in a @do@ block.
+{- | An use case of a keyword in a @do@ block.
 
 A keyword is a delimited continuation,
 which can be either ad-hoc polymorphic or not.
 
 Don't create custom instances of 'Dsl' for keywords.
 Instead, create 'PolyCont' for both your custom keywords and built-in keywords. 
+
+==== __Examples__
+
+>>> :set -XTypeFamilies
+>>> :set -XMultiParamTypeClasses
+>>> :set -XFlexibleInstances
+>>> :set -XFlexibleContexts
+>>> :set -XRebindableSyntax
+>>> :set -XTypeApplications
+>>> import qualified Prelude
+>>> import Prelude hiding ((>>), (>>=), return)
+>>> import Control.Dsl
+>>> import Control.Dsl.State
+>>> import Control.Dsl.Yield
+>>> import Control.Dsl.Return
+>>> import Data.Void
+
+>>> :{
+f = do
+  Yield "foo"
+  config <- Get @Bool
+  when config $ do
+    Yield "bar"
+    ($ ())
+  return "baz"
+:}
+
+>>> :type f
+f :: (PolyCont (Yield [Char]) r (),
+      PolyCont (Return [Char]) r Void, PolyCont Get r Bool) =>
+     r
+
+>>> f True :: [String]
+["foo","bar","baz"]
+
+>>> f False :: [String]
+["foo","baz"]
+
+>>> :{
+instance PolyCont (Yield String) (IO ()) () where
+  runPolyCont (Yield a) = (Prelude.>>=) (putStrLn $ "Yield " ++ a)
+:}
+
+>>> :{
+instance PolyCont Get (IO ()) Bool where
+  runPolyCont Get f = putStrLn "Get" Prelude.>> f False
+:}
+
+>>> :{
+instance PolyCont (Return String) (IO ()) Void where
+  runPolyCont (Return a) _ = putStrLn $ "Return " ++ a
+:}
+
+>>> f :: IO ()
+Yield foo
+Get
+Return baz
 -}
 class Dsl k r a where
   (>>=) :: k r a -> r !! a
   (>>) :: k r a -> r -> r
   k >> a = k >>= const a
 
--- | Keywords based on ad-hoc polymorphic delimited continuation.
+-- | Keywords based on ad-hoc polymorphic delimited continuations.
 instance {-# OVERLAPPABLE #-} PolyCont k r a => Dsl k r a where
   (>>=) = runPolyCont
 
--- | Keywords based on monomorphic delimited continuation.
+-- | Keywords based on monomorphic delimited continuations.
 instance Dsl Cont r a where
   (>>=) = runCont
 
